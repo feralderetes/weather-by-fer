647e9a74e69a0558e8a31fa403a56eba0b915be1
Fer Alderetes <feralderetes@gmail.com>
2024-09-19 16:24:07 +0200

    Added css properties

--- src/App.js
+++ src/App.js
@@ -4,27 +4,32 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <form>
-          <div className="row">
-            <div className="col-10">
-              <input
-                type="search"
-                placeholder="Search city 🔎"
-                className="form-control"
-              ></input>
+      <div className="container p-4">
+        <header className="pb-3">
+          <form>
+            <div className="row">
+              <div className="col-10">
+                <input
+                  type="search"
+                  placeholder="Search city 🔎"
+                  className="form-control search-bar"
+                  autoFocus="on"
+                ></input>
+              </div>
+              <div className="col-2">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn search-btn"
+                ></input>
+              </div>
             </div>
-            <div className="col-2">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary"
-              ></input>
-            </div>
-          </div>
-        </form>
-        <Weather />
-        <footer>
+          </form>
+        </header>{" "}
+        <main>
+          <Weather />
+        </main>
+        <footer className="mt-4 pt-3">
           Coded by Fer Alderetes 👩🏻‍💻, open-sourced on Github and hosted on
           Netlify
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -3,24 +3,40 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather row">
+    <div className="Weather row mt-3">
       <div className="current-weather col-6">
         <h1>Berlin,</h1>
         <h2>Germany</h2>
-        <div>22° Icon</div>
-        <div>Clear sky</div>
+        <div className="temperature">
+          <span className="temperature-value">22</span>
+          <span className="temperature-degrees">°</span>
+          <span className="temperature-icon">🌤️</span>
+        </div>
+        <div className="condition">Clear sky</div>
       </div>
       <div className="additional-info col-6">
         <div className="row">
-          <div className="col-6">Feels Like: 20°</div>
-          <div className="col-6">Humidity: 74%</div>
+          <div className="col-5 additional-info-box m-1 p-2">
+            Feels Like <br />
+            <strong>20°</strong>
+          </div>
+          <div className="col-5 additional-info-box m-1 p-2">
+            Humidity <br />
+            <strong>74%</strong>
+          </div>
         </div>
         <div className="row">
-          <div className="col-6">Wind: 5.36 km/h</div>
-          <div className="col-6">Air pressure: 1027 hPa</div>
+          <div className="col-5 additional-info-box m-1 p-2">
+            Wind <br />
+            <strong>5.36 km/h</strong>
+          </div>
+          <div className="col-5 additional-info-box m-1 p-2">
+            Air pressure <br />
+            <strong>1027 hPa</strong>
+          </div>
         </div>
       </div>
-      <div>Last updated: Wednesday 12:32</div>
+      <div className="last-updated mt-2">Last updated: Wednesday 12:32</div>
     </div>
   );
 }
